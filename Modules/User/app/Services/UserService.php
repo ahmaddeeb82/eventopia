c872dcffbe5c36d305145a5cbaede35c2a92b4c4
SarahNabhan <sarahhefaa@gmail.com>
2024-05-04 20:58:32 +0300

    Added User Service

--- Modules/User/app/Services/UserService.php
+++ Modules/User/app/Services/UserService.php
@@ -2,10 +2,26 @@
 
 namespace Modules\User\app\Services;
 
+use Illuminate\Support\Facades\Hash;
+
 
 
 class UserService {
 
     
+    public $repository;
+
+    public function __construct($repository)
+    {
+        $this -> repository = $repository;
+    }
     
+
+    public function create($userInfo){
+        
+        $userInfo['password'] = Hash::make(['password']); 
+
+        $this -> repository -> create($userInfo); 
+
+    }
 }
