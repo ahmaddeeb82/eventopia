b5b8b492da17f2378d63a830358cf19c1a2c069b
SarahNabhan <sarahhefaa@gmail.com>
2024-05-10 12:44:48 +0300

    Edited User Controller (Added Functions "register+addUser+login")

--- Modules/User/app/Http/Controllers/UserController.php
+++ Modules/User/app/Http/Controllers/UserController.php
@@ -6,8 +6,10 @@ use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
 use Modules\User\app\Services\UserService;
+use Modules\User\Http\Requests\LoginRequest;
 use Modules\User\Http\Requests\RegisterRequest;
 use Modules\User\app\Repositories\UserRepository;
 
@@ -15,12 +17,52 @@ class UserController extends Controller
 {
     use ApiResponse;
 
-    public function create(RegisterRequest $request){
-        (new UserService(new UserRepository())) -> create($request -> except(['confirm_password']));
+    /*
+    Register User from the app
+    */
+
+    public function register(RegisterRequest $request){
+
+        $user_object = new UserService(new UserRepository());
+
+        $user = $user_object -> create($request -> except(['confirm_password']));
+
+        $user_object->assignRoleUser($user, 'User');
+
         return $this -> sendResponse(
             200,
             __('auth.create_user'),
+            $user_object->createToken($user)
         );
 
+   }
+
+   /*
+    add organizer or hall owner from the dashboard
+    */
+
+   public function addUser(RegisterRequest $request){
+
+        $user_object = new UserService(new UserRepository());
+
+        $user = $user_object -> create($request -> except(['confirm_password','role']));
+
+        $user_object->assignRoleUser($user , $request -> role);
+
+        return $this -> sendResponse(
+            200,
+            __('auth.create_user'),
+            $user_object->createToken($user)
+        );
+   }
+
+
+   public function login(LoginRequest $request){
+        
+    $user_object = new UserService(new UserRepository());
+
+    $user = $user_object -> login($request);
+
+    
    }
 }
