51fe4d4c92d677e68420f76704a9a0685538ae62
Deeb <deebahmad2023@gmail.com>
2024-05-18 17:50:30 +0300

    Edited Date Formatter

--- app/Traits/DateFormatter.php
+++ app/Traits/DateFormatter.php
@@ -13,16 +13,10 @@ trait DateFormatter {
         return $duration; 
     }
 
-    public function calcDurationForReservatio($start_date, $end_date) {
-        $start_date = Carbon::parse($start_date);
-        $end_date = Carbon::parse($end_date);
-        $start_hour = Carbon::parse($start_date->format('H:m:s'));
-        $end_hour = Carbon::parse($end_date->format('H:m:s'));
-        $duration = $end_hour->diffInHours($start_hour);
-        return [
-            'start_date' => $start_date->format('Y-m-d'),
-            'end_date' => $end_date->format('Y-m-d'),
-            'duration' => $duration
-        ];
+    public function calcDurationForReservation($start_time, $end_time) {
+        $start_time = Carbon::parse($start_time);
+        $end_time = Carbon::parse($end_time);
+        $duration = $end_time->diffInHours($start_time);
+        return $duration;
     }
 }
