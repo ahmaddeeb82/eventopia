0624edf0899f849cc580f15a550d9bd5e3f183ab
SarahNabhan <sarahhefaa@gmail.com>
2024-06-01 17:55:10 +0300

    Added Resources Reservation (Publice - Private)

---
+++ Modules/Reservation/app/Transformers/ReservationPublicResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Modules\Reservation\Transformers;
+
+use Illuminate\Http\Request;
+use Modules\Reservation\Models\Reservation;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ReservationPublicResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     */
+    
+    public function toArray(Request $request): array
+    {
+        return [
+            'category' => $this -> category,
+            'description' => $this -> description,
+            'photo' => json_decode($this -> photo),
+            'name' => $this -> name,
+            'address' => $this -> address,
+            'ticket_price' => $this -> ticket_price,
+        ];
+    }
+}
