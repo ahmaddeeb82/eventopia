9dfba68a91126a55b086038eed90cf0d529a94be
SarahNabhan <sarahhefaa@gmail.com>
2024-07-04 23:04:53 +0300

    Added Route for Send Date Time

--- Modules/Reservation/routes/api.php
+++ Modules/Reservation/routes/api.php
@@ -18,11 +18,12 @@ Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
     Route::apiResource('reservation', ReservationController::class)->names('reservation');
 });
 
-Route::middleware('localizeApi') -> controller(ReservationController::class)
+Route::middleware('localizeApi', 'auth:sanctum') -> controller(ReservationController::class)
 ->prefix('reservation')
 ->group(function(){
     Route::post('addInfo', 'addInfo');
     Route::post('addPhoto','addPhoto');
     Route::get('getInfo', 'getInfo');
     Route::post('addTickets','addTickets');
+    Route::get('dateRes','dateRes');
 });
