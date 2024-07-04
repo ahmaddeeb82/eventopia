5fde4e4269f8414829028c6d18c82c78c72e1067
SarahNabhan <sarahhefaa@gmail.com>
2024-07-04 23:01:59 +0300

    Added Function for Category (Repository-Interface)

--- Modules/Reservation/app/Repositories/ExtraPublicEventsRepository.php
+++ Modules/Reservation/app/Repositories/ExtraPublicEventsRepository.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Reservation\app\Repositories;
 
+use Modules\Reservation\Models\Category;
 use Modules\Reservation\Models\PublicEvent;
 use Modules\Reservation\Models\PublicEventReservation;
 use Modules\Reservation\Models\Reservation;
@@ -12,6 +13,12 @@ class ExtraPublicEventsRepository{
 
         return new PublicEvent($extraPublicEvents);
     
+    }
+
+    public function addCategory($categry){
+
+        return new Category($categry);
+
     }
 
 }
--- Modules/Reservation/app/Repositories/Interfaces/ExtraPublicEventsRepositoryInterface.php
+++ Modules/Reservation/app/Repositories/Interfaces/ExtraPublicEventsRepositoryInterface.php
@@ -5,5 +5,7 @@ namespace Modules\Reservation\app\Repositories\Interfaces;
 interface ExtraPublicEventsRepositoryInterface{
     public function add($extraPublicEvents);
 
+    public function addCategory($categry);
+    
     //public function tickets($tickets);
 }
