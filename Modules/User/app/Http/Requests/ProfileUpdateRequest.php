f77f9b10edc78d94104760ea205d43371fa2e9be
Deeb <deebahmad2023@gmail.com>
2024-07-30 13:35:09 +0300

    Edited Update Profile

--- Modules/User/app/Http/Requests/ProfileUpdateRequest.php
+++ Modules/User/app/Http/Requests/ProfileUpdateRequest.php
@@ -12,12 +12,12 @@ class ProfileUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'first_name' => 'sometimes',
-            'last_name' => 'sometimes',
-            'username' => 'sometimes|unique:users,username,'.auth()->user()->id.',|regex:/^[A-Za-z][A-Za-z0-9_]{7,29}$/',
-            'email' =>'sometimes|unique:users,email,'.auth()->user()->id.'|email',
-            'address' => 'sometimes',
-            'phone_number' => 'sometimes|unique:users,phone_number,'.auth()->user()->id.'|regex:/^(09)[345689][0-9]{7}$/',
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'username' => 'required|unique:users,username,'.auth()->user()->id.',|regex:/^[A-Za-z][A-Za-z0-9_]{7,29}$/',
+            'email' =>'required|unique:users,email,'.auth()->user()->id.'|email',
+            'address' => 'required',
+            'phone_number' => 'required|unique:users,phone_number,'.auth()->user()->id.'|regex:/^(09)[345689][0-9]{7}$/',
         ];
     }
 
